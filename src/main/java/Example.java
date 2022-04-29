e8a5cc60594571198686f99d45a95f19b268e772
mistryparas <84007528+mistryparas@users.noreply.github.com>
2022-04-29 17:06:12 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - Demo for internal Teams. Blue deployment";
+        return "Hello World - Demo for internal Teams. demo 2";
     }
 
     public static void main(String[] args) throws Exception {
